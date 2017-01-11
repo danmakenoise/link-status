74e572974af12878f71f80eaaeae25add75e0260
Orry Baram <orry@mic.com>
2017-01-11 14:36:41 -0500

    fixes tests

--- test/format-line.test.js
+++ test/format-line.test.js
@@ -13,7 +13,7 @@ test('formatLine() with shouldDisplaySource = false', function (t) {
 
   t.equal(
     formatLine(validLine),
-    '\n\t✔ miclint\n',
+    'miclint',
     'it returns the name of the linked module when the line is valid'
   );
 
@@ -25,13 +25,19 @@ test('formatLine() with shouldDisplaySource = true', function (t) {
   var validLine = '42697913        8 lrwxr-xr-x    1 dan              staff                  62 Jan  8 15:40 ./node_modules//miclint -> ../.nvm/versions/node/v6.9.1/lib/node_modules/miclint';
 
   t.equal(
-    formatLine(invalidLine, true),
+    formatLine(invalidLine, {shouldDisplaySource: true}),
     '',
     'it returns an empty string when the line is invalid'
   );
 
   t.equal(
-    formatLine(validLine, true),
+    formatLine(validLine, {shouldDisplaySource: true}),
+    'miclint\n  ╚═══ ../.nvm/versions/node/v6.9.1/lib/node_modules/miclint',
+    'it returns the name of the linked module and its source when the line is valid'
+  );
+
+  t.equal(
+    formatLine(validLine, {shouldDisplaySource: true, shouldPrettyPrint: true}),
     '\n\t✔ miclint\n\t  ╚═══ ../.nvm/versions/node/v6.9.1/lib/node_modules/miclint\n',
     'it returns the name of the linked module and its source when the line is valid'
   );
